4ee0667ddf72cae846fe61ca653a93ecab752028
wayne-pham <62787240+wayne-pham@users.noreply.github.com>
2021-12-06 11:57:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from pandas.core.frame import DataFrame
 import tensorflow_decision_forests as tfdf
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+import math
 
 # set dataset
-trainDataset = pd.read_csv("AmazonDataset3.csv")
+trainDataset = pd.read_csv("AmazonDatasetModified.csv")
 
 # convert to tensorflow datasets
-trainTFDS = tfdf.keras.pd_dataframe_to_tf_dataset(trainDataset, label="Helpfulness")
+trainTFDS = tfdf.keras.pd_dataframe_to_tf_dataset(trainDataset, label="ReviewTextWordCount", max_num_classes=200)
 
 # Train Model
 model = tfdf.keras.RandomForestModel()
-model.fit(trainTFDS)
+# model.compile(metrics=["accuracy"])
+
+model.fit(x=trainTFDS)
+
+model.make_inspector().evaluation()
 
 print(model.summary())
+
+tfdf.model_plotter.plot_model(model)
